efdc13d37ca95d59f8814988c419ca9a115c3165
Rodrigo Vilela <rpiresvilela@gmail.com>
2020-04-16 18:06:36 -0300

    Colocado no index três exemplos de slides

--- inc/header.php
+++ inc/header.php
@@ -6,7 +6,7 @@
       <a class="navbar-brand" href="index.php">
         <img src="https://slack.global.ssl.fastly.net/9427/img/icons/app-256.png" alt="Logo" width="30" height="30"> Terra Patrum</a>
 
-
+<!-- Ítens da barra de navegação -->
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
