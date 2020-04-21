8b616ed6932bb136d509d7de0d00eb3395222979
Rodrigo Vilela <rpiresvilela@gmail.com>
2020-04-21 17:27:54 -0300

    Acréscimo de mais arquivos para as páginas

---
+++ inc/barnav/produtos.php
@@ -0,0 +1,1 @@
+iuyfhgu6ygfui
--- inc/header.php
+++ inc/header.php
@@ -6,7 +6,7 @@
       <a class="navbar-brand" href="index.php">
         <img src="https://slack.global.ssl.fastly.net/9427/img/icons/app-256.png" alt="Logo" width="30" height="30"> Terra Patrum</a>
 
-<!-- Ítens da barra de navegação -->
+      <!-- Ítens da barra de navegação -->
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -16,10 +16,11 @@
             <a class="nav-link" href="#">Início <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Produtos</a>
+            <a class="nav-link" href="inc\barnav\produtos.php?p=paginaProdutos">Produtos</a>
+            
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contatos</a>
+            <a class="nav-link" href="inc\barnav\contato.php">Contatos</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#" data-toggle="modal" data-target="#modalLogin">Login</a>
